0b874e088335b60de8a13361a9c00202b35dadb4
art7235 <Art7235B@gmail.com>
2025-01-31 23:28:49 +0300

    добавил html, но выводит ошибку

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "hello world"
+    return render_template("index.html")
 
 
 @app.route("/about")
 def about():
-    return "aaaaaaaaaaaayyyyyyy"
+    return render_template("about.html")
+
+
+@app.route("/user/<string:name>/<int:id>")
+def user(name, id):
+    return name + str(id)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
